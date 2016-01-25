6c42eebcca58db3ba49fadeaef35a340526f4b65
Marco Testa <mrc.tst@gmail.com>
2016-01-25 14:47:25 +0100

    Refactoring

--- src/main/java/it/mondogrua/countapp/CountApp.java
+++ src/main/java/it/mondogrua/countapp/CountApp.java
@@ -12,40 +12,42 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class CountApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         AbstractCount count = new DateCount();
-        
         ObservableCount observableCount = new ObservableCount(count);
-        
+
         JavaUtilsObservableAdapter javaUtilsObservableAdapter = new JavaUtilsObservableAdapter(observableCount);
         observableCount.add(javaUtilsObservableAdapter);
-        
+
         PropertyObservableAdapter propertyObservableAdapter = new PropertyObservableAdapter(observableCount);
         observableCount.add(propertyObservableAdapter);
 
-        createCountViewContainer(primaryStage, "JavaFX DateCount Example", new JFXBuilder(observableCount, propertyObservableAdapter));
-        createCountViewContainer(new Stage(), "Alternative JavaFX DateCount Example", new AltJFXBuilder(observableCount, propertyObservableAdapter));
-        createCountViewContainer(new Stage(), "SWING DateCount Example", new SwingBuilder(observableCount, javaUtilsObservableAdapter));
+        addScene(primaryStage, "JavaFX DateCount Example", new JFXBuilder(observableCount, propertyObservableAdapter));
+        addScene(new Stage(), "Alternative JavaFX DateCount Example", new AltJFXBuilder(observableCount, propertyObservableAdapter));
+        addScene(new Stage(), "SWING DateCount Example", new SwingBuilder(observableCount, javaUtilsObservableAdapter));
     }
 
-    private void createCountViewContainer(Stage stage, String lable, Builder builder) {
-        CountViewContainer countViewContainer = new CountViewContainer();
-        countViewContainer.openOn(builder);
-        
-        Scene scene = builder.getScene(350, 100);
-
+    private void addScene(Stage stage, String lable, Builder builder) {
+        Scene scene = buildScene(builder);
         stage.setScene(scene);
         stage.setTitle(lable);
         stage.show();
     }
 
+    private Scene buildScene(Builder builder) {
+        builder.addPane();
+        builder.addDisplayBoxOn(1, 1);
+        builder.addIncrementButtonOn(0, 2);
+        builder.addDecrementButtonOn(1, 2);
+        builder.addResetButtonOn(2, 2);
+        return builder.getScene(350, 100);
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-
 }
