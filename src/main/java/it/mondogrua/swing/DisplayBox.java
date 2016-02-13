60b241e5bd3460584a1e2670939bc8de946382fb
marco <mrc.tst@gmail.com>
2016-02-13 18:45:06 +0100

    Use PluggableAdaptor to retrieve value to display

--- src/main/java/it/mondogrua/swing/DisplayBox.java
+++ src/main/java/it/mondogrua/swing/DisplayBox.java
@@ -1,12 +1,12 @@
 package it.mondogrua.swing;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+
+import it.mondogrua.utils.PluggableAdaptor;
 
 @SuppressWarnings("serial")
 public class DisplayBox extends JLabel implements Observer {
@@ -35,22 +35,7 @@ public class DisplayBox extends JLabel implements Observer {
 
     private String getContentFrom(Object model, String action) {
 
-        Method method;
-        try {
-            method = model.getClass().getMethod(action);
-            Object ret = method.invoke(model, new Object[] {});
-            return ret.toString();
-        } catch (SecurityException e) {
-            // ...
-        } catch (NoSuchMethodException e) {
-            // ...
-        } catch (IllegalAccessException e) {
-            // ...
-        } catch (IllegalArgumentException e) {
-            // ...
-        } catch (InvocationTargetException e) {
-            // ...
-        }
-        return "";
+        return new PluggableAdaptor(model, action, new Object[] {}).execute()
+                .toString();
     }
 }
