27f25d9cff8154f7f6017aeefa8b8497ccb49c35
marco <mrc.tst@gmail.com>
2016-02-13 18:31:29 +0100

    Remove useless member

--- src/main/java/it/mondogrua/javafx/AltDisplayBox.java
+++ src/main/java/it/mondogrua/javafx/AltDisplayBox.java
@@ -7,19 +7,16 @@ import javafx.scene.control.Label;
 
 public class AltDisplayBox extends Label {
 
-    private final ChangeListener<? super String> observer;
-
     public AltDisplayBox(SimpleStringProperty observable) {
-        observer = new ChangeListener<Object>() {
+        observable.addListener(new ChangeListener<Object>() {
 
             @Override
             public void changed(ObservableValue<?> observable, Object oldValue,
                     Object newValue) {
                 setText(newValue.toString());
             }
-        };
+        });
 
-        observable.addListener(observer);
         setText(observable.getValueSafe());
     }
 }
