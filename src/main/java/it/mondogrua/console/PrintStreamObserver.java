3c8f73e1cb82e6cf31aa68fb71d98caae78f0d05
marco <mrc.tst@gmail.com>
2016-02-13 11:56:56 +0100

    input with Enter

--- src/main/java/it/mondogrua/console/PrintStreamObserver.java
+++ src/main/java/it/mondogrua/console/PrintStreamObserver.java
@@ -1,6 +1,9 @@
 package it.mondogrua.console;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.Reader;
 
 import it.mondogrua.count.Count;
 import it.mondogrua.count.CountObserver;
@@ -8,9 +11,12 @@ import it.mondogrua.count.CountObserver;
 public class PrintStreamObserver implements CountObserver {
 	private Count count;
 	private PrintStream out;
+	private Thread keyReader;
 
 	public PrintStreamObserver(PrintStream out) {
 		this.out = out;
+		this.keyReader = new Thread(new KeyReader());
+		this.keyReader.start();
 	}
 	
 	public void setSubject(Count aCount) {
@@ -29,5 +35,31 @@ public class PrintStreamObserver implements CountObserver {
 
 	private String retrieveValue() {
 		return this.count.getCountValue().toString();
+	}
+	
+	private class KeyReader implements Runnable {
+
+		@Override
+		public void run() {
+			read();
+		}
+		private void read() {
+			Reader entrada = new InputStreamReader(System.in);
+			try {
+				boolean stop = false;
+				while (!stop) {
+					char c = (char)entrada.read();
+					if (c == 'r') {
+						count.reset();
+					} else if (c == '+') {
+						count.increment();
+					} else if (c == '-') {
+						count.decrement();
+					}
+				}
+			} catch(IOException ex){
+				
+			}
+		}
 	}
 }
