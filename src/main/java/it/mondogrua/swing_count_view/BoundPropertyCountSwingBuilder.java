5b452bcd4e197e4e3fd83132386fdc7897dd66b4
Marco Testa <mrc.tst@gmail.com>
2016-03-08 09:03:03 +0100

    min or changes

--- src/main/java/it/mondogrua/swing_count_view/BoundPropertyCountSwingBuilder.java
+++ src/main/java/it/mondogrua/swing_count_view/BoundPropertyCountSwingBuilder.java
@@ -20,10 +20,8 @@ public class BoundPropertyCountSwingBuilder extends CountSwingBuilder {
     public void addDisplayBoxOn(int x, int y) {
         JLabel label = new JLabel();
         PropertyChangeListenerJLableAdapter jLableAdapter = makeJLableAdapter(label);
-
         BoundPropertyValueModel valueModel = new BoundPropertyValueModel(count,
                 new ValueModelAdaptor(count, Count.GET_VALUE_METHOD));
-
         jLableAdapter.bind(valueModel);
         add(label, x, y);
     }
