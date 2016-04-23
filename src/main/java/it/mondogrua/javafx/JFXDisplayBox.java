bc12223674a9be07aba763aa8387d3ba7c3d787f
marco <mrc.tst@gmail.com>
2016-04-23 10:30:23 +0200

    Remove unnecessary Bindings.convert

--- src/main/java/it/mondogrua/javafx/JFXDisplayBox.java
+++ src/main/java/it/mondogrua/javafx/JFXDisplayBox.java
@@ -1,12 +1,11 @@
 package it.mondogrua.javafx;
 
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Label;
 
 public class JFXDisplayBox extends Label {
 
     public void bind(SimpleStringProperty property) {
-        textProperty().bind(Bindings.convert(property));
+        textProperty().bind(property);
     }
 }
